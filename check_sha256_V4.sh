99a593ec91bc81bb6be36053e8d2775891871cd7
LostByteSoft <mad_mike_41@hotmail.com>
2021-02-24 10:37:56 -0500

    Add files via upload

--- check_sha256_V4.sh
+++ check_sha256_V4.sh
@@ -24,8 +24,9 @@ echo ---------------------------------------------------------------------------
 
 exist="$file".sha256""
 	if test -f "$exist"; then
-	echo "exist, autoread"
+	echo "An associated sha256 files exist, autoread"
 	#read -r firstline<"$file.sha256"
+	sha256file="$file".sha256
 
 else
 	echo "OPTIONAL - Associated sha256 file NOT exist, must select OR create (*.sha256 *.txt)"
@@ -38,7 +39,7 @@ echo ---------------------------------------------------------------------------
 {
 if test -z "$sha256file"
 	then
-		echo "You don't have selected a file. You will create a new *.sha256 file"
+		echo "You don't have selected a file. You will create a new *.sha256 file."
 		echo Press ENTER to continue the creation of a .sha256 file.
 		read name
 			cd /"$(dirname "${VAR1}")"
@@ -61,11 +62,14 @@ if test -z "$sha256file"
 			read name
 			sha256file="${file}.sha256"
 	else
-	echo "An sha256 file is selected and continue"
+	echo "An sha256 file is selected and continue (no creation)."
 fi
 }
 
+fi
+
 echo -----------------------------------------------------------------------------
+
 
 	# read all lines
 	echo "All the lines in the file you provide."
@@ -80,52 +84,47 @@ echo ---------------------------------------------------------------------------
 	#put the good line in the variable firstline
 	firstline="`grep "$(basename "${VAR2}")" "$sha256file"`"
 
-fi
-
 	echo Debug echo, you need 3 variable here.
-	echo file="$file"
-	echo sha256file="$sha256file"
-	echo firstline="$firstline"
-	#echo Press ENTER !
-	#read name
+	echo Var1 file = "$file"
+	echo Var2 sha256file = "$sha256file"
+	echo Var3 firstline = "$firstline"
 	
 	#watch if sha is include in file you specified
 	{
 	if test -z "$firstline"
 		then
 			echo "No matching lines found in file you specified."
-			echo Press ENTER to continue!
+			echo Press ENTER to EXIT!
 			read name
+			exit
 		else
 			echo "File selected and continue."
 			fi
 	}
 
-echo -----------------------------------------------------------------------------
+	echo -----------------------------------------------------------------------------
 
 	#two file have ben provided.
 	export VAR2="$file"
 	#echo dirname = "$(dirname "${VAR2}")"
 	#echo basename = "$(basename "${VAR2}")"
-	echo -----------------------------------------------------------------------------
 	sha256sum "$file" | awk '{print $1}' > "/dev/shm/"$(basename "${VAR2}")".sha256"
 	read -r calsum<"/dev/shm/"$(basename "${VAR2}")".sha256"
+	echo "Your selected file for hash is : "
+	echo $file
 	echo "The ckeck sum calculated is :"
 	echo calsum = "$calsum"
 	echo "$calsum $(basename "${VAR2}")" > "/dev/shm/"$(basename  "${VAR2}")".sha256"
 	echo ------------------------
-	echo "Your selected file for hash is : "
-	echo $file
-	echo ------------------------
 	echo "Your selected sha256 file is : (Only if you have selected one) "
 	echo "$sha256file"
-	echo -------------------------------------------------------------
+	echo -----------------------------------------------------------------------------
 	calsum2=""$calsum"  "$(basename "${VAR2}")""
 	#echo calsum... = "$calsum"  "$(basename "${VAR2}")"
 	echo Visually compare these two lines !
 	echo calsum2.. = "$calsum2"
 	echo firstline = "$firstline"
-	echo ------------------------
+	echo -----------------------------------------------------------------------------
 	
 	red=`tput setaf 1`
 	green=`tput setaf 2`
@@ -153,22 +152,17 @@ exit
 echo --- End of bash ---
 
             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-   Version 3.14159265358979323846264338327950288419716939937510582
-                          February 2021
-
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-   
-  	 Everyone is permitted to copy and distribute verbatim
- 		or modified copies of this license document.
+                        Version 3.1415926535
+                          February 2022
 
 	  As is customary and in compliance with current global and
 	interplanetary regulations, the author of these pages disclaims
 	all liability for the consequences of the advice given here,
 	in particular in the event of partial or total destruction of
-	the material, Loss of rights to the manufacturer's warranty,
+	the material, Loss of rights to the manufacturer warranty,
 	electrocution, drowning, divorce, civil war, the effects of
 	radiation due to atomic fission, unexpected tax recalls or
-	    encounters with extraterrestrial beings 'elsewhere.
+	    encounters with extraterrestrial beings elsewhere.
 
       LostByteSoft no copyright or copyleft we are in the center.
 
