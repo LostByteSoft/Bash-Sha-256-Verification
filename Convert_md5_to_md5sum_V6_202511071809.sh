f214ca97dd644638c1876063bd23664f097f1786
LostByteSoft <mad_mike_41@hotmail.com>
2026-01-11 08:40:48 -0500

    Add files via upload

---
+++ Convert_md5_to_md5sum_V6_202511071809.sh
@@ -0,0 +1,440 @@
+#!/bin/bash
+#!/usr/bin/ffmpeg
+
+	echo "-------------------------===== Start of bash ====-------------------------"
+	printf '\033[8;27;90t'	## minimum of 90 for graphics.
+	version=2025-11-07_Friday_18:09:18
+	hashtype=md5
+
+	## "0 or 1 , 0 deactivated , 1 activated ## All general variables must be 0 or 1"
+		findsubfolders=0
+		lowercase=0
+		detox=0
+		automatic=1
+		debug=0
+		debugcore=0
+		minimize=0
+		maximize=0
+		reseize=1
+		noquit=1
+
+	SCRIPT_NAME=$(basename "$0")
+	echo -ne "\033]0;$SCRIPT_NAME\a"
+	printf '\033[8;20;90t'		## minimum of 90 for graphics.
+	echo
+
+## -------------------------=========== SEPARATOR =============-------------------------
+	## Time, host name, file name, pid.
+		start=$SECONDS
+		now=$(date +"%Y-%m-%d_%A_%H:%M:%S")	## time now
+		hostname=$(cat /etc/hostname)
+		#hostname="${hostname%-pc}"
+		me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+		id=$(echo $PPID)			## current process id of the bash process
+	## General purposes variables. Needed before program to random variables.
+		logs=1		## Logging
+	## All general variables must be 0, DO NOT CHANGE.
+		part=0		## Don't change this value.
+		primeerror=0	## Ending error detector, do not change.
+		error=0		## Test error, do not change.
+	## Auto-generated variables. DO NOT CHANGE.
+		random=$(shuf -i 131072-524288 -n 1)	# Used for temp folders. A big number hard to guess for security reasons. 128-512
+		random2=$RANDOM
+	## Colors codes
+		red=$(tput setaf 1)
+		green=$(tput setaf 2)
+		yellow=$(tput setaf 11)
+		blue=$(tput setaf 12)
+		orange=$(tput setaf 9)
+		reset=$(tput sgr0)
+	## Name of the pc you are running this bash.
+		comuputerrunning=$(hostname)
+		comuputerrunning=${comuputerrunning//-pc/}
+
+## -------------------------=========== SEPARATOR =============-------------------------
+
+	echo
+	echo "${green}  ░▒▓█▓▒░      ░▒▓██████▓▒░ ░▒▓███████▓▒░▒▓████████▓▒░ "
+	echo "${green}  ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░     "
+	echo "${green}  ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░     "
+	echo "${green}  ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░   ░▒▓█▓▒░     "
+	echo "${green}  ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░     "
+	echo "${green}  ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░     "
+	echo "${green}  ░▒▓████████▓▒░▒▓██████▓▒░░▒▓███████▓▒░   ░▒▓█▓▒░     ${reset}"
+	echo
+	echo "${green}	  ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓████████▓▒░ "
+	echo "${green}	  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░        "
+	echo "${green}	  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░        "
+	echo "${green}	  ░▒▓███████▓▒░ ░▒▓██████▓▒░   ░▒▓█▓▒░   ░▒▓██████▓▒░   "
+	echo "${green}	  ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░        "
+	echo "${green}	  ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░        "
+	echo "${green}	  ░▒▓███████▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓████████▓▒░ ${reset}"
+	echo
+	echo "${green}		   ░▒▓███████▓▒░░▒▓██████▓▒░░▒▓████████▓▒░▒▓████████▓▒░ "
+	echo "${green}		  ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░     "
+	echo "${green}		  ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░     "
+	echo "${green}		   ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░    ░▒▓█▓▒░     "
+	echo "${green}			 ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░     "
+	echo "${green}			 ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░     "
+	echo "${green}		  ░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░         ░▒▓█▓▒░  ${reset}"
+	echo
+
+echo -------------------------=========== SEPARATOR =============-------------------------
+	echo "Lead-in Version 6.09. LostByteSoft ; https://github.com/LostByteSoft"
+	echo
+	echo "${blue}█████${reset} Activated $me"
+	echo
+	echo "Version : $version"
+	echo
+	echo "NEVER remove dual ## in front of lines. Theses are code annotations."
+	echo "You can test / remove single # for testing purpose."
+	echo
+	echo "Current time : $now"
+	echo
+	echo "You are running this script from : $comuputerrunning"
+	echo
+	echo "Running software file : $(dirname "$0")/"
+	echo
+	echo "Read me for this file : (EULA at the end of file, open in text.)"
+	echo "By LostByteSoft, no copyright or copyleft. https://github.com/LostByteSoft"
+	echo "Don't hack paid software, free software exists and does the job better."
+	echo
+	echo "Debug data : id=$id part=$part primeerror=$primeerror error=$error random=$random random2=$random2 logs=$logs"
+	echo "	findsubfolders=$findsubfolders lowercase=$lowercase detox=$detox automatic=$automatic debug=$debug"
+	echo "	debugcore=$debugcore minimize=$minimize maximize=$maximize reseize=$reseize noquit=$noquit"
+	echo
+
+echo -------------------------=========== SEPARATOR =============-------------------------
+	echo "Color codes / Informations."
+	echo
+	echo   "${blue}	████████████████      INFORMATION(S)      ████████████████ ${reset}"
+	echo  "${green}	████████████████     ALL OK / ACTIVE      ████████████████ ${reset}"
+	echo "${yellow}	████████████████   ATTENTION / INACTIVE   ████████████████ ${reset}"
+	echo "${orange}	████████████████                          ████████████████ ${reset}"
+	echo    "${red}	████████████████   FATAL ERROR / OFFLINE  ████████████████ ${reset}"
+	echo
+
+part=$((part+1))
+echo "-------------------------===== Section $part =====-------------------------"
+## Simple function small bar to wait 3 sec. This bar does work with debian 12 & 13. Version 1.05
+
+	functionsmallbar() {
+		speed=0.2
+		echo -ne ${yellow}'	TimeToQuit |                        |    (0%)\r'${reset}
+		sleep $speed
+		echo -ne ${yellow}'	TimeToQuit |  ██                    |   (10%)\r'${reset}
+		sleep $speed
+		echo -ne ${yellow}'	TimeToQuit |  ████                  |   (20%)\r'${reset}
+		sleep $speed
+		echo -ne ${yellow}'	TimeToQuit |  ██████                |   (30%)\r'${reset}
+		sleep $speed
+		echo -ne ${yellow}'	TimeToQuit |  ████████              |   (40%)\r'${reset}
+		sleep $speed
+		echo -ne ${yellow}'	TimeToQuit |  ██████████            |   (50%)\r'${reset}
+		sleep $speed
+		echo -ne ${yellow}'	TimeToQuit |  ████████████          |   (60%)\r'${reset}
+		sleep $speed
+		echo -ne ${yellow}'	TimeToQuit |  ██████████████        |   (70%)\r'${reset}
+		sleep $speed
+		echo -ne ${yellow}'	TimeToQuit |  ████████████████      |   (80%)\r'${reset}
+		sleep $speed
+		echo -ne ${yellow}'	TimeToQuit |  ██████████████████    |   (90%)\r'${reset}
+		sleep $speed
+		echo -ne ${yellow}'	TimeToQuit |  ████████████████████  |  (100%)\r'${reset}
+		sleep $speed
+		echo -ne '\n'
+		}
+
+## -------------------------===== Separator =====-------------------------
+echo "The core/code program. Version 6.017"
+	echo
+	echo "	Convert incompatible ${hashtype} file(s) to correct format."
+	echo
+
+## -------------------------===== Separator =====-------------------------
+
+	## 0 or 1 , 0 deactivated , 1 activated ## All general variables must be 0 or 1
+	debugcore=0
+	
+	touch "/dev/shm/$random.txt"
+
+	if [ "$debugcore" -eq "0" ]; then
+		## normal operation
+		printf '\033[8;20;90t'		# will resize the window.
+	else
+		## debugcore
+		printf '\033[8;40;90t'		# will resize the window.
+		echo debugcore=$debugcore
+		echo
+		echo "/dev/shm/$random.txt"
+		echo
+		echo hashtype=${hashtype}
+		echo
+		noquit=1
+		reseize=0
+	fi
+
+## -------------------------===== Separator =====-------------------------
+echo "Select a file using dialog !"
+	echo
+
+	file="$(zenity --file-selection --filename=$HOME/ --file-filter="*.md5 *.md5sum *.TXT" --title="Select a file, ${hashtype} format supported")"	## File select.
+
+	count=`ls -1 "$file" 2>/dev/null | wc -l`
+	if [ $count -ge "1" ]; then
+		echo Count : $count
+		echo
+		echo "You have selected :"
+		echo "$file"
+		echo
+		fi
+
+## -------------------------===== Separator =====-------------------------
+
+### file or folder
+	if test -z "$file"	## for cancel on zenity
+		then
+			echo "You click CANCEL !"
+			echo
+			echo "${yellow}█████████████████████ NO DATA TO PROCESS █████████████████████${reset}"
+			echo
+			read -n 1 -s -r -p "Press any key to EXIT"
+			echo
+			exit
+			fi
+
+echo -------------------------========================-------------------------
+## Version 4,00 Input_Directory_Output
+	## Set working path.
+		BASEDIR=$(dirname "$0")
+		dir=$(pwd)
+	## file or folder selected
+		export VAR="$file"
+		echo
+	## directory section
+		INPUT="$(dirname "${VAR}")"	
+	## Output file name
+		name=`echo "$file" | rev | cut -f 2- -d '.' | rev` ## remove extension
+		name1=`echo "$(basename "${VAR}")" | rev | cut -f 2- -d '.' | rev` ## remove extension
+		#echo "Output name bis 1 : "$name1""
+		name1="$name1"
+
+	## -------------------------===== Separator =====-------------------------
+
+	if [ "$debugcore" -eq "1" ]; then
+		echo "Input name, directory and output name : (Debug helper)"
+		echo
+		echo Basedir : "$BASEDIR"
+		echo "Working dir : "$dir""
+		echo Input file : "$file"
+		echo "Get the last Folder : ${INPUT##*/}"
+		echo Base directory : "$(dirname "${VAR}")"
+		echo Base name: "$(basename "${VAR}")"
+		echo
+		echo "Output name ext : "$name""
+		echo "Output name1 bis : "$name1""
+		echo
+		echo -------------------------========================-------------------------
+		fi
+
+## -------------------------===== Separator =====-------------------------
+
+	echo
+	echo "${blue}	█████████████████ WORKING WAIT !!! ${hashtype} ███████████████████${reset}"
+	echo
+
+	cp "$file" "/dev/shm/$random.txt"
+
+	if [ "$debugcore" -eq "1" ]; then
+		cat "/dev/shm/$random.txt"
+		echo
+		fi
+
+	## -------------------------===== Separator =====-------------------------
+	##Removing string(s).
+
+	echo 'Converted: Hashtypesums  *..\ filename.all TO Hashtypesums  ./ filename.all'
+	echo
+	sed -i 's/\*\.\.\\/\.\//g' /dev/shm/$random.txt		## Replace *..\ to ./
+	echo 'Removing all lines beginning with ; :'
+	echo
+	sed -i '/^;/d' /dev/shm/$random.txt			## Remove all lines beginning with ;
+	#echo "Removing all lines with md5 :"
+	#echo
+	#sed -i '/\.md5/d' /dev/shm/$random.txt
+	echo "Removing all lines with hash sum :"
+	echo
+	sed -i '/\.md5sum/d' /dev/shm/$random.txt
+	sed -i '/\.256sum/d' /dev/shm/$random.txt
+	sed -i '/\.384sum/d' /dev/shm/$random.txt
+	sed -i '/\.512sum/d' /dev/shm/$random.txt
+	
+	cp "/dev/shm/$random.txt" "$(dirname "${VAR}")"/"$name1".${hashtype}sum
+	cat "$(dirname "${VAR}")"/"$name1".${hashtype}sum
+
+	echo
+	echo "${green}	█████████████████ FINISH ███████████████████${reset}"
+	echo
+
+## -------------------------===== Separator =====-------------------------
+## Temp file eraser
+
+	if [ "$debugcore" -eq "1" ]; then
+		read -n 1 -s -r -p "DEBUG - Before erase - Press any key to continue."
+		echo
+		echo
+		fi
+
+	rm "/dev/shm/$random.txt" 2> /dev/null
+
+part=$((part+1))
+echo "-------------------------===== Section $part =====-------------------------"
+echo "Software lead out. Version 7.34"
+	echo
+
+## -------------------------===== Start of eula =====-------------------------
+	functioneula() {
+		printf '\033[8;33;90t'		## Will resize the window.
+		echo
+		echo "End-user license agreement (eula)"
+		echo
+	 	echo "JUST DO WHAT THE F*** YOU WANT WITH THE PUBLIC LICENSE"
+		echo	 	
+	 	echo "Version 3.1415926532 (January 2022)"
+		echo
+	 	echo "TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION"
+		echo
+		echo "Everyone is permitted to copy and distribute verbatim or modified copies of"
+	 	echo "this license document."
+		echo
+	 	echo "As is customary and in compliance with current global and interplanetary"
+	 	echo "regulations, the author of these pages disclaims all liability for the"
+	 	echo "consequences of the advice given here, in particular in the event of partial"
+	 	echo "or total destruction of the material, Loss of rights to the manufacturer"
+	 	echo "warranty, electrocution, drowning, divorce, civil war, the effects of radiation"
+	 	echo "due to atomic fission, unexpected tax recalls or encounters with"
+	 	echo "extraterrestrial beings elsewhere."
+		echo
+	 	echo "YOU MUST ACCEPT THESES TERMS OR NOTHING WILL HAPPEN."
+		echo
+	 	echo "LostByteSoft no copyright or copyleft we are in the center."
+		echo
+	 	echo "You can send your request and your Christmas wishes to this address:"
+		echo
+	 	echo "	Père Noël"
+	 	echo " 	Pôle Nord, Canada"
+	 	echo "	H0H 0H0"
+	 	echo
+	 	}
+
+## -------------------------===== Separator =====-------------------------
+	echo "Debug random : id=$id part=$part primeerror=$primeerror error=$error random=$random random2=$random2"
+	echo
+	echo "Debug : findsubfolders=$findsubfolders lowercase=$lowercase detox=$detox automatic=$automatic"
+	echo "	debug=$debug debugcore=$debugcore minimize=$minimize maximize=$maximize reseize=$reseize noquit=$noquit"
+	echo
+	echo -------------------------===== End of Bash ======-------------------------
+	echo
+	echo "Finish... with numbers of actions : $part"
+	echo "This script take $(( SECONDS - start )) seconds to complete."
+	echo "Started Time : $now"
+	date=$(date -d@$(( SECONDS - start )) -u +%H:%M:%S)
+	echo "Time needed: $date"
+	now5=$(date +"%Y-%m-%d_%A_%H:%M:%S")
+	echo "Current time : $now5"
+	echo
+
+## -------------------------===== Separator =====-------------------------
+
+	if [ "$logs" -eq "1" ]; then
+		echo " " >> /dev/shm/logs.txt
+		echo "	Time now : $now5" >> /dev/shm/logs.txt
+		echo "	Time needed : $date" >> /dev/shm/logs.txt
+		echo "	Started Time : $now" >> /dev/shm/logs.txt
+		echo "	Name of software : $me" >> /dev/shm/logs.txt
+		echo "	Debug random : id=$id part=$part primeerror=$primeerror error=$error random=$random random2=$random2" >> /dev/shm/logs.txt
+		echo "	Debug : findsubfolders=$findsubfolders lowercase=$lowercase detox=$detox automatic=$automatic debug=$debug debugcore=$debugcore \
+		minimize=$minimize maximize=$maximize reseize=$reseize noquit=$noquit" >> /dev/shm/logs.txt
+		echo "	File (If any used) : $file" >>/dev/shm/logs.txt
+		echo " " >>/dev/shm/logs.txt
+		echo "--------------------====== SEPARATOR ========--------------------" >>/dev/shm/logs.txt
+		fi
+
+## -------------------------===== Separator =====-------------------------
+## Exit, wait or auto-quit.
+
+	echo -ne "\033]0;FINISH-$SCRIPT_NAME\a"
+
+	if [ "$primeerror" -ge "1" ]; then
+		printf '\033[8;22;90t'		## Will resize the window.
+		echo "${red}████████████████████████████████████████████${reset}	${yellow}████████████████████████████████████████${reset}"
+		echo "${red}██                                        ██${reset}	${yellow}██                                    ██${reset}"
+		echo "${red}██     Unknown entry event... ERROR(S)    ██${reset}	${yellow}██       Time needed : $date       ██${reset}"
+		echo "${red}██                                        ██${reset}	${yellow}██                                    ██${reset}"
+		echo "${red}████████████████████████████████████████████${reset}	${yellow}████████████████████████████████████████${reset}"
+		echo
+		echo "Numbers of error(s) : $primeerror"
+		echo
+		echo "Terminated : $me"
+		echo
+		noquit=1
+	else
+
+		printf '\033[8;19;90t'		## Will resize the window.
+		echo "${green}████████████████████████████████████████${reset}	${blue}████████████████████████████████████████${reset}"
+		echo "${green}██                                    ██${reset}	${blue}██                                    ██${reset}"
+		echo "${green}██         NO errors detected.        ██${reset}	${blue}██       Time needed : $date       ██${reset}"
+		echo "${green}██                                    ██${reset}	${blue}██                                    ██${reset}"
+		echo "${green}████████████████████████████████████████${reset}	${blue}████████████████████████████████████████${reset}"
+		echo
+		echo "Terminated : $me"
+		echo
+	fi
+
+	## -------------------------===== Separator =====-------------------------
+	if [ "$noquit" -eq "1" ]; then
+		if [ "$primeerror" -eq "0" ]; then
+				echo "${blue}	█████████████████ NO exit activated ███████████████████${reset}"
+			else
+				echo "${orange}	█████████████████ NO exit activated ███████████████████${reset}"
+			fi
+		echo
+		while true; do 
+			echo "Press ANYKEY to show EULA, ENTER or SPACE to EXIT." 
+			read -r -n 1 -s input_key
+			if [[ -n "input_key" ]]; then 
+				if [[ "input_key" == " " || -z "$input_key" ]]; then 
+					break 
+				else
+					functioneula
+					read -n 1 -s -r -p "Press any key to EXIT." 
+					echo
+					break
+				fi 
+			else
+					break 
+				fi 
+			done
+			echo
+			fi
+
+	## -------------------------===== Separator =====-------------------------
+	if [ "$debug" -eq "1" ]; then
+		debug
+		echo "${blue}		█████ DEBUG WAIT | Program finish. █████${reset}"
+		echo
+		read -n 1 -s -r -p "Press any key to EXIT or press X to close this windows !"
+		echo
+		echo
+		exit 0
+		fi
+
+	## -------------------------===== Separator =====-------------------------
+
+	echo "${green}	███████████████ Finish, quit in 3 seconds █████████████████${reset}"
+	echo
+	functionsmallbar
+	echo
+	exit 0
+
+## -------------------------===== End of file =====-------------------------
